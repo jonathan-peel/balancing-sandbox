09e3433c6322ca2d872536d28bf46a860e64e9b1
jonathan-peel <jonathan.n.peel@gmail.com>
2024-07-08 16:09:10 +0200

    Some successful model runs

--- cluster/run.sh
+++ cluster/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #SBATCH --ntasks=6
-#SBATCH --time=4:00:00
+#SBATCH --time=24:00:00
 #SBATCH --job-name="calliope6"
 #SBATCH --mem-per-cpu=50G
 #SBATCH --mail-type=BEGIN,END
 
-calliope run balancing-act-model/national/model.yaml --save_netcdf=results.nc --scenario=tm001,time-series-for-all-years,model-duration-1955
-
+module load gurobi/10.0.3
+calliope run balancing-act-model/national/model.yaml \
+--save_netcdf=results_daily.nc --scenario=tm001,time-series-for-all-years,model-duration-1955
